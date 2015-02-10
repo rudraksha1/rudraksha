42aa5a8c7955c5f1a9f6fe15f11412d73cef1a71
rudraksha1 <root1@mactekiiMac.local>
2015-02-10 11:24:48 +0800

    ff
    ff

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Hello World");
-		}
+		System.out.println("Hello World");
 	}
 }
